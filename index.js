5a6ab8ac3583ab318ad7957fefdcd6e76ed8f784
Rohan Kumar <rohankmr414@gmail.com>
2020-03-24 19:39:13 +0530

    added config file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = "!"
+
 const TOKEN = process.env.TOKEN;
 
 client.on('ready', () => {
@@ -10,7 +12,9 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.author.bot) return;
 
-    if (msg.content === 'ping') {
+    if (!msg.content.startsWith(prefix)) return;
+
+    if (msg.content.startsWith(`${prefix}ping`)) {
         msg.reply('Pong!');
     }
 });
